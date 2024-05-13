a2eb71ce1f038c90d4e3af7c3e26e8eda2d0da77
Krasimir Kargov <krasikargov0703@gmail.com>
2024-05-13 20:02:20 +0300

    update

--- Converter.cpp
+++ Converter.cpp
@@ -2,6 +2,29 @@
 #include <vector>
 #include <string>
 #include "Converter.h"
+#include "CellInterface.h"
 
+    CellInterface<int>* Converter::toInt(const std::string& value)
+    {
 
+    }
 
+    CellInterface<double>* Converter::toDouble(const std::string& value)
+    {
+
+    }
+
+    bool Converter::isString(const std::string& value)
+    {
+
+    }
+
+    bool Converter::isNull(const std::string& value)
+    {
+
+    }
+
+    void Converter::parseLineToParam(const std::string& line, std::vector<std::string>& parameters)
+    {
+        
+    }
--- Converter.h
+++ Converter.h
@@ -2,8 +2,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <CellInterface.h>
 
 class Converter
 {
-    
+    static CellInterface<int>* toInt(const std::string& value);
+    static CellInterface<double>* toDouble(const std::string& value);
+    static bool isString(const std::string& value);
+    static bool isNull(const std::string& value);
+
+    static void parseLineToParam(const std::string& line, std::vector<std::string>& parameters);
 };
