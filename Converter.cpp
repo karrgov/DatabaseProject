6d3a6b8671dcb7e74c90c446d51f89053812bf65
Krasimir Kargov <krasikargov0703@gmail.com>
2024-05-14 09:46:22 +0300

    update

--- Converter.cpp
+++ Converter.cpp
@@ -2,11 +2,64 @@
 #include <vector>
 #include <string>
 #include "Converter.h"
+#include "Cell.h"
 #include "CellInterface.h"
 
     CellInterface<int>* Converter::toInt(const std::string& value)
     {
+        CellInterface<int>* wrong = new Cell<int>(0, false);
 
+        if(value.size() == 0)
+        {
+            return wrong;
+        }
+
+        int start = 0;
+        bool isNegative = false;
+
+        if(value[0] == '+')
+        {
+            start++;
+        }
+        else if(value[0] == '-')
+        {
+            start++;
+            isNegative = true;
+        }
+
+        int result = 0;
+        bool in = false;
+
+        for(int i = start; i < value.size(); i++)
+        {
+            char current = value[i];
+
+            if(current >= '0' && current <= '9')
+            {
+                in = true;
+                result = result * 10 + current - '0';
+            }
+            else
+            {
+                return wrong;
+            }
+        }
+
+        if(in == false)
+        {
+            return wrong;
+        }
+
+        if(isNegative)
+        {
+            result = result * (-1);
+        }
+        else
+        {
+            result = result * 1;
+        }
+        
+        return new Cell<int>(result, true);
     }
 
     CellInterface<double>* Converter::toDouble(const std::string& value)
@@ -26,5 +79,5 @@
 
     void Converter::parseLineToParam(const std::string& line, std::vector<std::string>& parameters)
     {
-        
+
     }
